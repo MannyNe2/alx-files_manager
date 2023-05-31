6bec5d1e3c7043dbd559fa6fd8fbaa5d44950efd
Manny Negussie <102585213+MannyNe2@users.noreply.github.com>
2023-05-31 18:39:55 +0300

    add tests

---
+++ tests/1-apps.test.js
@@ -0,0 +1,50 @@
+import {
+  expect, use, should, request,
+} from 'chai';
+import app from "../server";
+import dbClient from "../utils/db";
+import chaiHttp from 'chai-http';
+
+use(chaiHttp);
+should();
+
+// General Application Endpoints
+
+describe('Testing Application Status Endpoints', () => {
+  describe('GET /status', () => {
+    it('Returns the status of redis and mongo connection?', async () => {
+      const response = await request(app).get('/status').send();
+      const body = JSON.parse(response.text);
+
+      expect(body).to.eql({ redis: true, db: true });
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+
+  describe('GET /stats', () => {
+    before(async () => {
+      await dbClient.usersCollection.deleteMany({});
+      await dbClient.filesCollection.deleteMany({});
+    });
+
+    it('Returns number of users and files in db 0 for this one?', async () => {
+      const response = await request(app).get('/stats').send();
+      const body = JSON.parse(response.text);
+
+      expect(body).to.eql({ users: 0, files: 0 });
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('Returns number of users and files in db 1 and 2 for this one?', async () => {
+      await dbClient.usersCollection.insertOne({ name: 'Larry' });
+      await dbClient.filesCollection.insertOne({ name: 'image.png' });
+      await dbClient.filesCollection.insertOne({ name: 'file.txt' });
+
+      const response = await request(app).get('/stats').send();
+      const body = JSON.parse(response.text);
+
+      expect(body).to.eql({ users: 1, files: 2 });
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+});
