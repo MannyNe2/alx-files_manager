f94f77aa55d36f570cba00ef4cf7b890a7b9c203
Manny Negussie <102585213+MannyNe2@users.noreply.github.com>
2023-05-31 16:48:13 +0300

    update route and add usersController

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,28 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from "../controllers/UsersController";
 
 function initRoutes(app) {
   const router = express.Router();
-  app.use('/', router);
+  app.use("/", router);
 
   // App Controller
 
   // should return if Redis is alive and if the database is alive
-  router.get('/status', (req, res) => {
+  router.get("/status", (req, res) => {
     AppController.getStatus(req, res);
   });
 
   // should return the number of users and files in the database
-  router.get('/stats', (req, res) => {
+  router.get("/stats", (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  // User Controller
+
+  // should create a new user in DB
+  router.post("/users", (req, res) => {
+    UsersController.postNew(req, res);
   });
 }
 
